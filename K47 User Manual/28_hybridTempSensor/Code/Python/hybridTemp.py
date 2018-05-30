cff2296cc0cee55d0a7266deb83e44ad6982e94c
Nicholas Jackiw <nicholasjackiw@gmail.com>
2018-05-30 13:55:34 -0700

    temperature sensors
    - English & format revision for [analogTempSensor,
    digitalTempSensor(DS18B20), and hybridTempSensor].
    - renamed hybridTempSensor (from previous misnamed digitalTempsensor)
    per #28

--- K47 User Manual/28_hybridTempSensor/Code/Python/hybridTemp.py
+++ K47 User Manual/28_hybridTempSensor/Code/Python/hybridTemp.py
@@ -12,15 +12,15 @@ def init():
 def loop():	
     while True:
         global digitalVal
+        print 'Current analog value is %d'%  ADC0832.getResult(0)
         digitalVal = GPIO.input(TempSensor_DO_PIN)
         if(digitalVal == 1):
-            print 'DO is %d' % digitalVal
-            print "Temprature alarm ..."
-	    print 'Current analog value is %d'%  ADC0832.getResult(0)
-            time.sleep(0.2)
+            print "Temperature alarm ... threshold exceeded!"
         else:
-            pass
-
+        	pass
+        	
+	    time.sleep(0.2)
+        
 if __name__ == '__main__':
 	init()
 	try:
