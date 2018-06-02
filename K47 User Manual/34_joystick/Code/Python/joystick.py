2bdaa1b1dce678451fa9ef7ca730a39bd8a73797
Alion3064492356 <3064492356@qq.com>
2018-06-02 12:29:05 +0800

    revise joystick refer to your proposal

--- K47 User Manual/34_joystick/Code/Python/joystick.py
+++ K47 User Manual/34_joystick/Code/Python/joystick.py
@@ -11,33 +11,41 @@ import time
 
 btn = 15	
 
+xFlag = 0
+yFlag = 0
+
 def setup():
 	ADC0832.setup()										# Setup ADC0832
 	GPIO.setmode(GPIO.BOARD)							# Numbers GPIOs by physical location
 	GPIO.setup(btn, GPIO.IN, pull_up_down=GPIO.PUD_UP)	# Setup button pin as input an pull it up
-	global state
-	state = ['up', 'down', 'left', 'right']	
 
-def getResult():										#get joystick result
+def getResult():
+	#get joystick result
+	global xFlag, yFlag
 	if ADC0832.getResult(1) == 0:
-		return 1		#up
+		xFlag = 1		#up
 	if ADC0832.getResult(1) == 255:
-		return 2		#down
+		xFlag = 2		#down
 
 	if ADC0832.getResult(0) == 0:
-		return 3		#left
+		yFlag = 1		#left
 	if ADC0832.getResult(0) == 255:
-		return 4		#right
+		yFlag = 2		#right
 
 	if GPIO.input(btn) == 0:
 		print 'Button is pressed!'						# Button pressed
 
 def loop():
 	while True:
-		tmp = getResult()
-		if tmp != None:
-			print state[tmp - 1]
-                time.sleep(0.2)
+		getResult()
+		if xFlag == 1:
+			print 'up'
+		elif xFlag == 2:
+			print 'down'
+		if yFlag == 1:
+			print 'left'
+		elif yFlag == 2:
+			print 'right'
 
 def destory():
 	GPIO.cleanup()										# Release resource
